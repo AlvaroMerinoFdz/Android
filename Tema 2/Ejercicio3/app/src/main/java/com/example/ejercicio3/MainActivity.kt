6ababf88be62c3d0314e113b9c8fc64ed2cc7fa7
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2021-10-13 12:57:25 +0200

    Ejercicio 3 terminado, y proyecto del ejercicio4 creado

--- Tema 2/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
+++ Tema 2/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.ejercicio3
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var sumando1: EditText = findViewById(R.id.editTextValor1)
+        var sumando2: EditText = findViewById(R.id.editTextValor2)
+        var boton: Button = findViewById(R.id.btnBoton)
+        var resul : TextView = findViewById(R.id.txtResultado)
+
+
+
+        boton.setOnClickListener(){
+            var s1 = Integer.parseInt(sumando1.text.toString())
+            var s2 = Integer.parseInt(sumando2.text.toString())
+            var resultado = s1+s2
+            resul.text = "${getString(R.string.resul)} : $resultado"
+            Toast.makeText(this,"Sumando...", Toast.LENGTH_LONG).show()
+
+        }
     }
 }
