d8c2649931f21fda3f7557d5de47ed21d007bb53
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2021-10-14 15:39:54 +0200

    Proyecto 8, parte realizada en clase

--- Ejercicio8/app/src/main/java/com/example/ejercicio8/MainActivity.kt
+++ Ejercicio8/app/src/main/java/com/example/ejercicio8/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.ejercicio8
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+    }
+    @SuppressLint("WrongViewCast")
+    fun avanzaBarra(view: View){
+        var barra: SeekBar = findViewById(R.id.skHoras)
+        var numeroHoras: TextView = findViewById(R.id.txtHoras)
+        numeroHoras.text = barra.progress.toString()
     }
 }
