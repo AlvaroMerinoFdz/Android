fe776d6f7c16dc6d350a5ca7602550bfaa325c5f
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2021-10-21 12:56:10 +0200

    Ejercicio del dado y de adivinar numero completos

--- EjercicioDados/app/src/main/java/com/example/ejerciciodados/MainActivity.kt
+++ EjercicioDados/app/src/main/java/com/example/ejerciciodados/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,21 +18,24 @@ class MainActivity : AppCompatActivity() {
         var tuyo:Int = (1..6).random()
         var imagenOrdenador:ImageView = findViewById(R.id.imgOrdenador)
         var imagenTuya:ImageView = findViewById(R.id.imgTuya)
-        when(ordenador){
-            1 -> imagenOrdenador.setImageResource(R.drawable.dado1)
-            2 -> imagenOrdenador.setImageResource(R.drawable.dado2)
-            3 -> imagenOrdenador.setImageResource(R.drawable.dado3)
-            4 -> imagenOrdenador.setImageResource(R.drawable.dado4)
-            5-> imagenOrdenador.setImageResource(R.drawable.dado5)
-            else -> imagenOrdenador.setImageResource(R.drawable.dado6)
+        var resultado:TextView = findViewById(R.id.txtResultado)
+        elegirImagen(ordenador,imagenOrdenador)
+        elegirImagen(tuyo,imagenTuya)
+        when {
+            ordenador<tuyo -> resultado.text = "Has ganado"
+            ordenador>tuyo -> resultado.text = "Has perdido"
+            else -> resultado.text = "Empate"
+
         }
-        when(tuyo){
-            1 -> imagenTuya.setImageResource(R.drawable.dado1)
-            2 -> imagenTuya.setImageResource(R.drawable.dado2)
-            3 -> imagenTuya.setImageResource(R.drawable.dado3)
-            4 -> imagenTuya.setImageResource(R.drawable.dado4)
-            5-> imagenTuya.setImageResource(R.drawable.dado5)
-            else -> imagenTuya.setImageResource(R.drawable.dado6)
+    }
+    fun elegirImagen(numero:Int, imagen:ImageView){
+        when(numero){
+            1 -> imagen.setImageResource(R.drawable.dado1)
+            2 -> imagen.setImageResource(R.drawable.dado2)
+            3 -> imagen.setImageResource(R.drawable.dado3)
+            4 -> imagen.setImageResource(R.drawable.dado4)
+            5-> imagen.setImageResource(R.drawable.dado5)
+            else -> imagen.setImageResource(R.drawable.dado6)
         }
     }
 }
